d5a4ef31ae26f0eed09eb508b98cd2ef489a4297
gidmak <gideonmarkus@gmail.com>
2021-09-05 12:39:26 +0100

    Anonymous students scores

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AnonymousSchoolRoaster
 {
@@ -7,6 +8,48 @@ namespace AnonymousSchoolRoaster
         static void Main(string[] args)
         {
             Console.WriteLine("***** ANONYMOUS SCHOOL ROASTER *****");
+
+            Random numberOfStudents = new Random();
+            List<int> scores = new List<int>();
+
+            
+            for(int i = 0; i < numberOfStudents.Next(20,150); i++)
+            {
+                scores.Add(numberOfStudents.Next(1,100));
+            }
+
+            int firstTier = 0;
+            int secondTier = 0;
+            int failed = 0;
+            
+            for(int j = 0; j < scores.Count; j++)
+            {
+                if(scores[j] >= 70)
+                {
+                    firstTier++;
+                }
+                else if(scores[j] >= 50)
+                {
+                    secondTier++;
+                }
+                else
+                {
+                    failed++;
+                }
+                
+            }
+            Console.WriteLine("TOTAL STUDENTS: " + scores.Count);
+            Console.WriteLine("ABOVE 70: " + firstTier);
+            Console.WriteLine("BETWEEN 50 AND 69: " + secondTier);
+            Console.WriteLine("BELOW 50: " + failed);
+            //Console.WriteLine(examScores.Next(1,100));
+
+            int roll = 0;
+            foreach(int totalScore in scores)
+            {
+                roll++;
+                Console.WriteLine(roll+") " + totalScore);
+            }
         }
     }
 }
