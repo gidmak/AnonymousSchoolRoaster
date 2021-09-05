5e9a72a764ec429b086e8ee54f1afd55c90940e2
gidmak <gideonmarkus@gmail.com>
2021-09-05 07:37:36 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AnonymousSchoolRoaster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** ANONYMOUS SCHOOL ROASTER *****");
+        }
+    }
+}
